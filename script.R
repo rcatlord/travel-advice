c505cf670427cdf1baf8bd2b9de16a60608a418f
rcatlord <hcpartridge@uclmail.net>
2024-08-07 17:51:38 +0100

    Add reset button

--- script.R
+++ script.R
@@ -46,15 +46,14 @@ factpal <- colorFactor(palette = c("#E94F35","#FCB700","#C5D54E"),
                                   "Advise against all but essential travel",
                                   "Check travel advice before travelling"))
 
-  leaflet(world,
-          options = leafletOptions(
-            maxZoom = 5,
-            crs = leafletCRS(
-              crsClass = "L.Proj.CRS", code = "ESRI:53009",
-              proj4def = "+proj=moll +lon_0=0 +x_0=0 +y_0=0 +a=6371000 +b=6371000 +units=m +no_defs",
-              resolutions = c(65536, 32768, 16384, 8192, 4096, 2048)))) |>
-  #setView(0, 0, 2) |> 
-  #addProviderTiles(providers$CartoDB.Positron) |>
+map <- leaflet(world,
+                options = leafletOptions(
+                  maxZoom = 5,
+                  crs = leafletCRS(
+                    crsClass = "L.Proj.CRS", code = "ESRI:53009",
+                    proj4def = "+proj=moll +lon_0=0 +x_0=0 +y_0=0 +a=6371000 +b=6371000 +units=m +no_defs",
+                    resolutions = c(65536, 32768, 16384, 8192, 4096, 2048)))) |>
+  setView(0,0,1) |> 
   addPolygons(weight = 1, color = "#FFFFFF", opacity = 1, 
               fillColor = ~factpal(world$status), fillOpacity = 0.8,
               popup = paste0(
@@ -70,7 +69,14 @@ factpal <- colorFactor(palette = c("#E94F35","#FCB700","#C5D54E"),
             labels = c("Advise against all travel",
                        "Advise against all but essential travel",
                        "Check travel advice before travelling"), opacity = 0.8) |>
-  addControl(paste0("<strong>Latest FCDO travel advice</strong>"), position = "topright") |> 
+  addControl(paste0("<h2>Latest FCDO travel advice</h2>"), position = "topright", className = "map-title") |> 
   addGraticule(style = list(color = "#999", weight = 0.5, opacity = 1, fill = NA)) |> 
   addGraticule(sphere = TRUE, style = list(color = "#777", weight = 1, opacity = 0.25, fill = NA)) |> 
+  addEasyButton(easyButton(
+    icon = "ion-arrow-shrink",
+    title = "Reset view",
+    onClick = JS("function(btn, map){ map.setView([0,0],1); }")
+    )) |> 
   setMapWidgetStyle(list(background = "white"))
+
+map
